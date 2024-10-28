4fa6cf2963e5f2ffa0d6fc9cb80f712ff3c781e9
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-10-28 21:19:49 +0530

    algo done

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,9 @@
 import { createConsola } from 'consola'
 
 export const logger = createConsola({})
+
+export const newLine = (n: number) => {
+  for (let i = 0; i < n; i++) {
+    console.log('\n')
+  }
+}
---
+++ src/utils/readDir.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+// Function to generate the tree structure for a given directory
+export const generateTreeDiagrahm = (dirPath: string, indent: string = ''): void => {
+  try {
+    const items = fs.readdirSync(dirPath)
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i] as string
+      const itemPath = path.join(dirPath, item)
+      const isLastItem = i === items.length - 1
+
+      const connector = isLastItem ? '└── ' : '├── '
+      console.log(indent + connector + item)
+
+      if (fs.statSync(itemPath).isDirectory()) {
+        const nextIndent = indent + (isLastItem ? '    ' : '│   ')
+        generateTreeDiagrahm(itemPath, nextIndent)
+      }
+    }
+  } catch (err) {
+    console.error(`Error reading ${dirPath}: ${err}`)
+  }
+}
